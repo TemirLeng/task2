2404c8b70d52f531a981f61dd6351c13f14b642e
vijuap <vijuap.83@gmail.com>
2022-11-22 18:20:16 -0500

    Commited by vijayalaxmi in  src/main/java/Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -21,13 +21,43 @@ public class Task {
         /**
          * Count duplicate chars in given string, return true if there are duplicates
          */
-        return  false;
+        char[] ch1=example.toCharArray();
+        int[] freq=new int[ch1.length];
+        for(int i=0;i<ch1.length;i++) {
+            freq[i]=1;
+            for (int j=1+i;j<ch1.length;j++) {
+                if (ch1[i]==ch1[j] && ch1[j]!=' ') {
+                    freq[i]++;
+                    ch1[j]=' ';
+                }
+
+            }
+
+        }
+        for (int p=0;p<freq.length;p++) {
+            if (freq[p]>1) {
+                return true;
+            }
+        } return  false;
+
+
+
     }
 
     public static boolean isPalindrome(String word){
         /**
          * return true if given string is palindrome, else return false
          */
+        String rev="";
+
+        for (int i=word.length()-1;i>=0;i--) {
+            rev=rev+word.charAt(i);
+
+        }
+
+        if (word.equalsIgnoreCase(rev)) {
+            return true;
+        }
         return false;
     }
 
